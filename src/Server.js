2bec848efee9abf22dd8dbbe6604056d9416f5dc
MDTufan <tufanali702@gmail.com>
2023-10-08 18:26:37 -0700

    express

--- src/Server.js
+++ src/Server.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const express=require("express");
+const app=express();
+const morgan = require('morgan')
+const port=3001;
+app.use(morgan("dev"));
+app.get("/",(req,res)=>{
+    res.status(200).send('Hello i an hom page',
+    );
+});
+
+app.listen(port,()=>{
+    console.log(`Server is Runing http://localhost:${port}`);
+})
