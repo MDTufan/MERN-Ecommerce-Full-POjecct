35af8e66d3c2d725a5431bf660978f142500af1f
MDTufan <tufanali702@gmail.com>
2023-10-08 18:45:15 -0700

    mediwlwr

--- src/Server.js
+++ src/Server.js
@@ -3,8 +3,31 @@ const app=express();
 const morgan = require('morgan')
 const port=3001;
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+const Islogin=(req,res,next)=>{
+   console.log("Toli");
+   next();
+}
+
+
 app.get("/",(req,res)=>{
     res.status(200).send('Hello i an hom page',
+    );
+});
+app.get("/user",Islogin,(req,res)=>{
+    console.log("tufan");
+    res.status(200).send('Hello i an User page',
+    );
+});
+
+
+app.post("/",(req,res)=>{
+    res.status(200).send('Hello i an post route',
     );
 });
 
